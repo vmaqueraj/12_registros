1f4aa91f906aef08a3e3d16476ee03cb7782988d
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-06-24 10:14:20 -0500

    Desarrollo de estructura

--- libros.cpp
+++ libros.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+struct Libro{
+	string titulo;
+	string autor;
+	string editorial;
+	int anioPublicacion;
+};
+
+
 int main () {
+	int n;
+	Libro libros[100];
 	
+	cin>>n;
 	
+	for (int i=0; i<n; i++){
+		
+		getline(cin, libros[i].titulo);
+		getline(cin, libros[i].autor);
+		getline(cin, libros[i].editorial);
+		getline(cin, libros[i].anioPublicacion);
+		
+	}
 	
-	
+	for (int i=0; i<n; i++){
+		
+		
+	}
 	
 	return 0;
 }
