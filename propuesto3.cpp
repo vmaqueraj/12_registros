b4873166cdb9e387343b1a1478ce391eddf1d0af
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-06-28 14:03:08 -0500

    Propuesto 3: Estructura inicial del programa

---
+++ propuesto3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+struct Persona {
+	string nombre; 
+	int dia;
+	int mes;
+	int año;
+	
+};
+
+
+
+int main () {
+	
+	
+	
+	
+	
+	return 0;
+}
