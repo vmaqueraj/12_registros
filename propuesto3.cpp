822d43ed19834258eab79c22feb1c6f83cde12da
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-06-28 14:10:41 -0500

    Propuesto 3: Registro de personas

--- propuesto3.cpp
+++ propuesto3.cpp
@@ -22,7 +22,21 @@ int main () {
 	cin >> n;
 	cin.ignore();
 	
-	
+	for (int i=0; i<n; i++){
+		cout << endl << "Persona " << i+1 << endl;
+		cout << "Nombres: ";
+		getline(cin, personas[i].nombre);
+		cout << "Dia de nacimiento: ";
+		cin >> personas[i].dia;
+		cout << "Mes de nacimiento: ";
+		cin >> personas[i].mes;
+		cout << "Anio de nacimiento: ";
+		cin >> personas[i].anio;
+		cin.ignore();
+	}
+		
+		
+
 	
 	
 	return 0;
