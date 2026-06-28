1a52922c3f4461c61c9c82f920680d48bf84012b
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-06-27 20:20:36 -0500

    Propuesto 1: Identificación del mayor y menor sueldo

--- propuesto1.cpp
+++ propuesto1.cpp
@@ -11,6 +11,7 @@ struct Empleado{
 
 int main () {
 	int n;
+	int mayor, menor;
 	Empleado empleados[100];
 	
 	cout << "Ingrese la cantidad de empleados: ";
@@ -26,10 +27,24 @@ int main () {
 		cout << "Sueldo: ";
 		cin >> empleados[i].sueldo;
 		cin.ignore();
-		
-		
-		
 	}
+	
+	// Identificar el empleado con mayor y menor sueldo
+	
+	mayor = 0;
+	menor = 0;
+	
+	for(int i=1; i<n; i++){
+		if (empleados[i].sueldo > empleados[mayor].sueldo){
+			mayor = i;
+		}
+		if (empleados [i].sueldo < empleados[menor].sueldo){
+			menor = i;
+		}
+	}
+	
+	cout << empleados[mayor].nombres<<endl;
+	cout << empleados[menor].nombres<<endl;
 	
 	
 	
