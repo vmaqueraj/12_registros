0d44d76fc28997fa8e02255d0099711e37ab6fdb
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-06-28 14:05:07 -0500

    Propuesto 3: Solicitar la cantidad de personas

--- propuesto3.cpp
+++ propuesto3.cpp
@@ -8,14 +8,19 @@ struct Persona {
 	string nombre; 
 	int dia;
 	int mes;
-	int año;
+	int anio;
 	
 };
 
 
 
 int main () {
+	int n;
+	Persona personas[100];
 	
+	cout << "Ingrese la cantidad de personas a registrar: ";
+	cin >> n;
+	cin.ignore();
 	
 	
 	
