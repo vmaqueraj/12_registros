434fda6da260321229449370d09cc9f07d7d3465
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-06-27 20:34:37 -0500

    Propuesto 2: Estructura de registro y leer cantidad de personas

--- propuesto2.cpp
+++ propuesto2.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+struct Persona{
+	string nombres;
+	string dni;
+	int edad;
+};
+
+
 int main (){
+	int n;
+	Persona persona[100];
 	
-	
+	cout << "Ingrese la cantidad de personas a registrar: ";
+	cin >> n;
+	cin.ignore();
 	
 	
 	
