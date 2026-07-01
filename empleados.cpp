4cced6270760082f787e2168590012a4baaf8c14
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-01 15:59:41 -0500

    Estructura inicial

---
+++ empleados.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Empleado{
+	int num;
+	string nombre;
+	int v[12];
+	int salario;
+	int suma;
+};
+
+int main (){
+	
+
+
+
+
+
+
+	return 0;
+}
+
