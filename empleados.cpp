27c64570c1e0ec41d1bf314aa947b3b310afac46
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-01 16:22:37 -0500

    Registro de datos de cada empleado

--- empleados.cpp
+++ empleados.cpp
@@ -11,7 +11,29 @@ struct Empleado{
 };
 
 int main (){
+	int n;
+	Empleado empleados[100];
+
+	cout << "Ingrese la cantidad de empleados a registrar: ";
+	cin >> n;
+	cin.ignore();
+
+	for (int i=0; i<n; i++){
+		cout << endl << "Empleado " << i+1 << endl;
+		cout << "Numero de empleado: ";
+		cin >> empleados[i].num;
+		cin.ignore();
+		cout << "Nombres: ";
+		getline (cin, empleados[i].nombre);
 	
+		cout << "Ingrese la venta de cada mes" << endl;	
+		for (int j=0; j<12; j++){
+			cin >> empleados[i].v[j];
+		}
+		cout << "Salario total: ";
+		cin >> empleados[i].salario;
+		cin.ignore();
+	}
 
 
 
