0e1966e9ca2efeb3222a04f958713f6e130f2e7e
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-01 16:49:12 -0500

    Encargado 1: Estructura inicial

---
+++ contactos.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct ContactoEmail {
+	string nombres; 
+	string sexo;
+	int edad;
+	string email;
+};
+
+
+int main (){
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
