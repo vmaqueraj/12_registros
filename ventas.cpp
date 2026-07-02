12ae0c640541b8254e0941dc0cf74db9c2e949f1
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-01 19:27:36 -0500

    Encargado 2: Estructura inicial

---
+++ ventas.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Producto {
+	string nombre;
+	float precio;
+};
+
+struct Venta {
+	int idVenta;
+	string producto;
+	int cantidad;
+	float precioTotal;
+};
+
+int main (){
+	
+	
+	
+	
+	
+	
+	return 0;
+}
